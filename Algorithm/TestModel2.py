09abebec0748b1fc2e781a26308ca2b9bceefbec
Angel <aas14@rice.edu>
2020-11-16 20:43:38 -0600

    add model tests

---
+++ Algorithm/TestModel2.py
@@ -0,0 +1,49 @@
+import numpy as np
+import tensorflow as tf
+import glob
+from PIL import Image
+
+thisdict = {0: "cardboard", 1: "glass", 2: "metal", 3: "paper", 4: "plastic", 5: "trash"}
+# Load the TFLite model and allocate tensors.
+interpreter = tf.lite.Interpreter(model_path="model.tflite")
+interpreter.allocate_tensors()
+
+# Get input and output tensors.
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+print("input details", input_details)
+print("output details", output_details)
+
+# Test the model on input data.
+image_list = []
+mylist =[]
+for filename in glob.glob(r'C:\Users\sotoa\PycharmProjects\RACOON\trashnet\data\dataset-resized\metal\*.jpg'):
+    im=Image.open(filename)
+    image_list.append(im)
+    mylist.append(filename)
+
+counter = 0
+for pic in image_list:
+#    print("type1", type(pic))
+    pic = np.array(pic, dtype=np.float32)
+#    print("type2", pic.dtype)
+    pic = pic[:, :, :3]
+#    print("type3", pic.dtype)
+    image_list[counter] = np.expand_dims(pic, axis=0)
+#    print("type4", pic.dtype)
+    counter += 1
+#    print("shape =", pic.shape)
+print("first image", type(image_list[3]), image_list[3].dtype)
+
+input_data = image_list
+mydict = {}
+mydict2 = {}
+
+for i in range(len(input_data)):
+    interpreter.set_tensor(input_details[0]['index'], input_data[i])
+    interpreter.invoke()
+    output = interpreter.get_tensor(output_details[0]['index'])
+    mydict[mylist[i]] = thisdict[np.argmax(output)]
+    mydict2[mylist[i]] = np.argmax(output)
+print("dict =", mydict.values())
---
+++ Algorithm/TestModel3.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+import tensorflow as tf
+assert tf.__version__.startswith('2')
+
+from tflite_model_maker import configs
+from tflite_model_maker import ExportFormat
+from tflite_model_maker import image_classifier
+from tflite_model_maker import ImageClassifierDataLoader
+from tflite_model_maker import model_spec
+
+import matplotlib.pyplot as plt
+
+image_path = r"C:\Users\sotoa\PycharmProjects\pythonProject1\trashnet\data\dataset-resized"
+
+data = ImageClassifierDataLoader.from_folder(image_path)
+print("data =", data, type(data))
+train_data, validation_data = data.split(0.9)
+#validation_data, test_data = rest_data.split(0.5)
+test_data = ImageClassifierDataLoader.from_folder(r"C:\Users\sotoa\PycharmProjects\RACOON\test-folder")
+
+model = image_classifier.create(train_data, model_spec=model_spec.efficientnet_lite0_spec, validation_data=validation_data)
+
+#model.summary()
+print("HaHA")
+loss, accuracy = model.evaluate(test_data)
+print("HaHa")
+
+#model.export(export_dir='.')
+
+#print("model", model.summary())
+print("loss", loss)
+print("accuracy", accuracy)
+
+#model None
+#loss 0.7411657571792603
+#accuracy 0.8695651888847351
