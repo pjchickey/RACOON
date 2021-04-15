a525a8642639b2148289e9fa6a1a4eedd4771b2f
Rene Carballo <rcarballo2222@gmail.com>
2021-04-14 20:49:09 -0500

    Add program flow for command handling

--- Sensors/arduino-pi_communication/talkwitharduino.py
+++ Sensors/arduino-pi_communication/talkwitharduino.py
@@ -2,7 +2,8 @@ import serial
 import time
 
 def commandArduino(command, retries=50):
-    COMMANDS = {0:("Sort to Trash", 0, 500), 1:("Sort to Recycling", 0, 500)}     #code: (desc, success exit code, timeout for completion in seconds)
+    status = 2 #2- No acknowledgement receieved; 1 - No completion code recieved; 0 - Command successfully executed 
+    COMMANDS = {0:("Sort to Trash", 0, 500), 1:("Sort to Recycling", 0, 500)}     #code: (desc, success exit code, number of retries for getting completion code)
     print(COMMANDS[command][0])
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     ser.flush()
@@ -24,9 +25,23 @@ def commandArduino(command, retries=50):
                     if(line != ""):
                         print(line)
                         if(line == COMMANDS[command][1]):
-                            break   #command complete
+                            status = 0  #command complete
+                            break   
                         else:
                             raise RuntimeError(f"Something went wrong when completing command {command}: {COMMANDS[command][0]}.")
+                if status == 0:
+                    break
+                else:
+                    raise TimeoutError(f"Maximum attempts of {COMMANDS[command][2]} exceeded for command {command}.")
+            else:
+                raise RuntimeError(f"Recieved bad response from Arduino: {line}")
+    if status == 0:
+        print("Task complete.")
+    elif status == 1:
+        raise RuntimeError(f"Command {command} acknowledged by Arduino, but task failed to complete.")
+    else:
+        raise RuntimeError(f"Command {command} failed successfully.")  
+            
             
     
 
