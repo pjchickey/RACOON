684be9fd4e00515123fd980a56952d13cff446fc
Patrick <pjc8@rice.edu>
2021-04-26 01:03:38 +0100

    servo bug fixes

--- Sensors/servo.py
+++ Sensors/servo.py
@@ -1,14 +1,15 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-pin = 18
+pin = 12
 
+GPIO.setwarnings(False) 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(pin, GPIO.OUT)
 pwm=GPIO.PWM(pin, 50)
 pwm.start(0)
 
-def SetAngle(angle):
+def setAngle(angle):
 	duty = angle / 18 + 2
 	GPIO.output(pin, True)
 	pwm.ChangeDutyCycle(duty)
@@ -16,7 +17,7 @@ def SetAngle(angle):
 	GPIO.output(pin, False)
 	pwm.ChangeDutyCycle(0)
 
-if __name__ == "main":
-    SetAngle(90) 
+if __name__ == "__main__":
+    setAngle(180)
     pwm.stop()
     GPIO.cleanup()
